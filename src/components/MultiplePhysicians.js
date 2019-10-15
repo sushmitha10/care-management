11ac714d77cacb0e54180f4afa7a4d3332def12b
ldp6242 <ldparks6242@gmail.com>
2019-10-15 15:49:27 -0500

    Created Initial Multiple Physicians Import View

--- src/components/MultiplePhysicians.js
+++ src/components/MultiplePhysicians.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import MaterialTable from 'material-table'
 
-export default function MultiplePhysicians() {
-    return (
-        <h2>Multiple Physicians</h2>
-    );
-}
+class MultiplePhysicians extends React.Component {
+    render() {
+      return (
+        <MaterialTable
+          columns={[
+            { title: 'First Name', field: 'first' },
+            { title: 'Last Name', field: 'last' },
+            { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+            {
+              title: 'Birth Place',
+              field: 'birthCity',
+              lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+            },
+          ]}
+          data={[
+            { first: 'Mehmet', last: 'Baran', birthYear: 1987, birthCity: 63 },
+            { first: 'Zerya Betül', last: 'Baran', birthYear: 1987, birthCity: 63 },
+          ]}
+          title="Multiple Physicians Import View"
+          detailPanel={rowData => {
+            return (
+              <iframe
+                width="100%"
+                height="315"
+                src="https://www.youtube.com/embed/C0DPdy98e4c"
+                frameborder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+              />
+            )
+          }}
+          onRowClick={(event, rowData, togglePanel) => togglePanel()}
+        />
+      )
+    }
+  }
+
+  export default MultiplePhysicians;
