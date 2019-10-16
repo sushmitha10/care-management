96d971a7125d1d88d98ea6e4ae7249e052935651
ldp6242 <ldparks6242@gmail.com>
2019-10-16 10:53:21 -0500

    added relevant date to multiple physicians view

--- src/components/MultiplePhysicians.js
+++ src/components/MultiplePhysicians.js
@@ -9,7 +9,8 @@ class MultiplePhysicians extends React.Component {
           columns={[
             { title: 'First Name', field: 'first' },
             { title: 'Last Name', field: 'last' },
-            { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+            { title: 'Practice', field: 'practice' },
+            { title: 'Available Spots', field: 'spots', type: 'numeric' },
             {
               title: 'Birth Place',
               field: 'birthCity',
@@ -17,8 +18,10 @@ class MultiplePhysicians extends React.Component {
             },
           ]}
           data={[
-            { first: 'Mehmet', last: 'Baran', birthYear: 1987, birthCity: 63 },
-            { first: 'Zerya Betül', last: 'Baran', birthYear: 1987, birthCity: 63 },
+            { first: 'Mehmet', last: 'Baran', practice: 'St. Vincents Ambulatory Care, Inc.', spots: '25', birthCity: 63 },
+            { first: 'Zerya Betül', last: 'Jones', practice: 'Via Christi Clinic Murdock', spots: '50', birthCity: 63 },
+            { first: 'Apryl', last: 'Johnson', practice: 'Via Christi Clinic Murdock', spots: '10', birthCity: 63 },
+
           ]}
           title="Multiple Physicians Import View"
           detailPanel={rowData => {
