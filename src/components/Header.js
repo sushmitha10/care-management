cd1d21ef2efc7dc0673dc067a6e19281c1bebf81
sushmitha10 <attapuram.sushmitha93@gmail.com>
2019-10-17 15:53:09 -0500

    file upload

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
 import Auth from "../Auth";
+import '../Styles.css';
 
 const useStyles = makeStyles(theme => ({
     font: {
--- src/components/fileUpload.js
+++ src/components/fileUpload.js
@@ -3,65 +3,72 @@ import {DropzoneDialog} from 'material-ui-dropzone'
 import Button from '@material-ui/core/Button';
 import '../Styles.css';
  
-export default class DropzoneDialogExample extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            open: false,
-            files: []
-        };
-    }
- 
-    handleClose() {
-        this.setState({
-            open: false
-        });
-    }
- 
-    handleSave(files) {
-        //Saving files to state for further use and closing Modal.
-        this.setState({
-            files: files, 
-            open: false
-        });
+export default class ProviderDropzoneDialog extends Component {
+	    constructor(props) {
+		            super(props);
+		            this.state = {
+				                open: false,
+				                files: [],
+				                formDataFileUploadBody: {},
+				                providers: {}
+				            };
+		        }
+	 
+	    handleClose() {
+		            this.setState({
+				                open: false
+				            });
+		        }
+	 
+	    handleSave(files, formDataFileUploadBody) { 
+		            formDataFileUploadBody = new FormData();
+		            formDataFileUploadBody.append('file',  files[0]);
 
-        fetch('https://reqres.in/api/users', {
-            // content-type header should not be specified!
-            method: 'POST',
-            body: files,
-        })
-            .then(response => response.json())
-            .then(success => {
-            // Do something with the successful response
-            console.log('File saved successfully');
-            window.location.href = "/multiplephysicians";
-            })
-            .catch(error => console.log(error)
-        );
-    }
- 
-    handleOpen() {
-        this.setState({
-            open: true,
-        });
-    }
+		            this.setState({
+				                
+				                files: files, 
+				                open: false,
+				                formDataFileUploadBody: formDataFileUploadBody
+				            });
+		            fetch('http://127.0.0.1:9091/acm/web/provider/multiple', {
+				                method: 'POST',
+				                body: formDataFileUploadBody
+				            }).
+			                then((response) => { 
+						                response.json().then((data) => { 
+									                    debugger;
+									                    this.setState({providers: data});
+									            } );
+						        });
 
-    render() {
-        return (
-            <div className="App-button">
-                <Button color="inherit" onClick={this.handleOpen.bind(this)}>
-                  Add Multiple Physicians
-                </Button>
-                <DropzoneDialog
-                    open={this.state.open}
-                    onSave={this.handleSave.bind(this)}
-                    acceptedFiles={['.xls', '.xlsx']}
-                    dropzoneText={' Drag and drop an excel file here or click to upload '}
-                    showPreviews={true}
-                    maxFileSize={5000000}
-                    onClose={this.handleClose.bind(this)}
-                />
-            </div>
-        );
+		        }
+	    handleOpen() {
+		            this.setState({
+				                open: true,
+				            });
+		        }
+
+	    render() {
+		            return (
+				                <div className="App-button">
+				                    <Button color="inherit" onClick={this.handleOpen.bind(this)}>
+				                      Add Multiple Physicians
+				                    </Button>
+				                    <DropzoneDialog
+				                        open={this.state.open}
+				                        onSave={this.handleSave.bind(this)}
+				                        acceptedFiles={['.xls', '.xlsx']}
+				                        dropzoneText={' Drag and drop an excel file here or click to upload '}
+				                        showPreviews={true}
+				                        maxFileSize={5000000}
+				                        onClose={this.handleClose.bind(this)}
+				                    />
+				                            <div className="App-grid">               
+				                            {/* <li key={this.state.providers.physicianLastName}>{this.state.providers.physicianLastName}</li> */}
+	                       
+	                </div>
+		            </div>
+		            
+		        );
     }
 }
