3cf846b3abdb7954740baec060c53a1a61c14c79
ldp6242 <ldparks6242@gmail.com>
2019-10-24 13:19:55 -0500

    Added Staging View

--- src/components/MultiplePhysicians.js
+++ src/components/MultiplePhysicians.js
@@ -1,61 +1,139 @@
 import React, { Component } from 'react'
 import MaterialTable from 'material-table'
+import { forwardRef } from 'react';
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowUpward from '@material-ui/icons/ArrowUpward';
+import Check from '@material-ui/icons/Check';
 import ChevronLeft from '@material-ui/icons/ChevronLeft';
 import ChevronRight from '@material-ui/icons/ChevronRight';
-import { forwardRef } from 'react';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
 import FirstPage from '@material-ui/icons/FirstPage';
 import LastPage from '@material-ui/icons/LastPage';
-import ArrowUpward from '@material-ui/icons/ArrowUpward';
-import SearchIcon from '@material-ui/icons/Search';
-
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
+import { withRouter } from 'react-router-dom';
 class MultiplePhysicians extends React.Component {
+    constructor(props) {
+      super(props);
+      console.log(Object.values(this.props.location.state.fileUploadData));
+      this.state = {
+        columns: [
+          { title: 'First Name', field: 'physicianFirstName' },
+          { title: 'Last Name', field: 'physicianLastName' },
+          { title: 'Middle Name', field: 'physicianMiddleName'},
+          { title: 'Physician Title', field: 'physicianTitle'},
+          { title: 'Practice', field: 'primaryPracticeName' },
+          { title: 'Available Spots', field: 'spots', type: 'numeric', initialEditValue: 250 },
+          // {
+          //   title: 'Birth Place',
+          //   field: 'birthCity',
+          //   lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+          // },
+        ]
+      }
+    }
+
+    componentDidUpdate() {
+      console.log(this.props);
+    }
+
+    // getPhysicianData() {
+    //   console.log(Object.values(this.props.location.state.fileUploadData));
+    //   if(this.props.location.state.fileUploadData) {
+    //     return Object.values(this.props.location.state.fileUploadData).map((val, index) => {
+    //       return (
+    //       <div key={index}>
+    //         { val.physicianLastName }
+    //       </div>
+    //       );
+    //     })
+    //   }
+    // }
+
     static tableIcons = {
-        FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
-        LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
-        NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-        PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
-        SortArrow: forwardRef((props, ref) => <ArrowUpward {...props} ref={ref} />),
-        Search: forwardRef((props, ref) => <SearchIcon {...props} ref={ref} />)
-      };
+      Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+      Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+      Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+      Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+      DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+      Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+      Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+      Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+      FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+      LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+      NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+      PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+      ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+      Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+      SortArrow: forwardRef((props, ref) => <ArrowUpward {...props} ref={ref} />),
+      ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+      ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+    };
 
     render() {
       return (
-        <MaterialTable
-          columns={[
-            { title: 'First Name', field: 'first' },
-            { title: 'Last Name', field: 'last' },
-            { title: 'Practice', field: 'practice' },
-            { title: 'Available Spots', field: 'spots', type: 'numeric' },
-            {
-              title: 'Birth Place',
-              field: 'birthCity',
-              lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
-            },
-          ]}
-          data={[
-            { first: 'Mehmet', last: 'Baran', practice: 'St. Vincents Ambulatory Care, Inc.', spots: '25', birthCity: 63 },
-            { first: 'Zerya Betül', last: 'Jones', practice: 'Via Christi Clinic Murdock', spots: '50', birthCity: 63 },
-            { first: 'Apryl', last: 'Johnson', practice: 'Via Christi Clinic Murdock', spots: '10', birthCity: 63 },
-
-          ]}
-          title="Multiple Physicians Import View"
-          icons={MultiplePhysicians.tableIcons}
-          detailPanel={rowData => {
-            return (
-              <iframe
-                width="100%"
-                height="315"
-                src="https://www.youtube.com/embed/C0DPdy98e4c"
-                frameborder="0"
-                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen
-              />
-            )
-          }}
-          onRowClick={(event, rowData, togglePanel) => togglePanel()}
-        />
+        <div>
+          <MaterialTable
+            title="Staging Preview"
+            icons={MultiplePhysicians.tableIcons}
+            columns={this.state.columns}
+            data={Object.values(this.props.location.state.fileUploadData)}
+            // options={{
+            //   selection: true
+            // }}
+            // actions={[
+            //   {
+            //     tooltip: 'Remove All Selected Users',
+            //     icon: 'delete',
+            //     onClick: (evt, data) => alert('You want to delete ' + data.length + ' rows')
+            //   }
+            // ]}
+            editable={{
+              onRowAdd: newData =>
+                new Promise((resolve, reject) => {
+                  setTimeout(() => {
+                    {
+                      const data = this.state.data;
+                      data.push(newData);
+                      this.setState({ data }, () => resolve());
+                    }
+                    resolve()
+                  }, 1000)
+                }),
+              onRowUpdate: (newData, oldData) =>
+                new Promise((resolve, reject) => {
+                  setTimeout(() => {
+                    {
+                      const data = this.state.data;
+                      const index = data.indexOf(oldData);
+                      data[index] = newData;
+                      this.setState({ data }, () => resolve());
+                    }
+                    resolve()
+                  }, 1000)
+                }),
+              onRowDelete: oldData =>
+                new Promise((resolve, reject) => {
+                  setTimeout(() => {
+                    {
+                      let data = this.state.data;
+                      const index = data.indexOf(oldData);
+                      data.splice(index, 1);
+                      this.setState({ data }, () => resolve());
+                    }
+                    resolve()
+                  }, 1000)
+                }),
+            }}
+          />
+        </div>
       )
     }
   }
 
-  export default MultiplePhysicians;
+  export default withRouter(MultiplePhysicians);
--- src/components/fileUpload.js
+++ src/components/fileUpload.js
@@ -2,73 +2,95 @@ import React, { Component } from 'react'
 import {DropzoneDialog} from 'material-ui-dropzone'
 import Button from '@material-ui/core/Button';
 import '../Styles.css';
+import MultiplePhysicians from './MultiplePhysicians';
+import {
+    BrowserRouter as Router,
+    Redirect
+  } from "react-router-dom";
  
 export default class ProviderDropzoneDialog extends Component {
-	    constructor(props) {
-		            super(props);
-		            this.state = {
-				                open: false,
-				                files: [],
-				                formDataFileUploadBody: {},
-				                providers: {}
-				            };
-		        }
-	 
-	    handleClose() {
-		            this.setState({
-				                open: false
-				            });
-		        }
-	 
-	    handleSave(files, formDataFileUploadBody) { 
-		            formDataFileUploadBody = new FormData();
-		            formDataFileUploadBody.append('file',  files[0]);
+	constructor(props) {
+		super(props);
+		this.state = {
+			open: false,
+			files: [],
+			formDataFileUploadBody: {},
+			providers: []
+		};
+	}
+	
+	handleClose() {
+		this.setState({
+			open: false
+		});
+	}
+	
+	handleSave(files, formDataFileUploadBody) { 
+		formDataFileUploadBody = new FormData();
+		formDataFileUploadBody.append('file',  files[0]);
 
-		            this.setState({
-				                
-				                files: files, 
-				                open: false,
-				                formDataFileUploadBody: formDataFileUploadBody
-				            });
-		            fetch('http://127.0.0.1:9091/acm/web/provider/multiple', {
-				                method: 'POST',
-				                body: formDataFileUploadBody
-				            }).
-			                then((response) => { 
-						                response.json().then((data) => { 
-									                    debugger;
-									                    this.setState({providers: data});
-									            } );
-						        });
+		this.setState({	
+			files: files, 
+			open: false,
+			formDataFileUploadBody: formDataFileUploadBody
+		});
 
-		        }
-	    handleOpen() {
-		            this.setState({
-				                open: true,
-				            });
-		        }
+		fetch('http://10.4.242.161:9091/acm/web/provider/multiple', {
+			method: 'POST',
+			body: formDataFileUploadBody
+		})
+		.then((response) => { 
+			response.json().then((data) => { 
+				this.setState({providers: data.providerMap});
+				// console.log(this.state.providers);
+			} );
+		})
+		.then(success => {
+			// Do something with the successful response
+			console.log('File uploaded successfully');
+		})
+		.catch(error => console.log(error));
+	}
 
-	    render() {
-		            return (
-				                <div className="App-button">
-				                    <Button color="inherit" onClick={this.handleOpen.bind(this)}>
-				                      Add Multiple Physicians
-				                    </Button>
-				                    <DropzoneDialog
-				                        open={this.state.open}
-				                        onSave={this.handleSave.bind(this)}
-				                        acceptedFiles={['.xls', '.xlsx']}
-				                        dropzoneText={' Drag and drop an excel file here or click to upload '}
-				                        showPreviews={true}
-				                        maxFileSize={5000000}
-				                        onClose={this.handleClose.bind(this)}
-				                    />
-				                            <div className="App-grid">               
-				                            {/* <li key={this.state.providers.physicianLastName}>{this.state.providers.physicianLastName}</li> */}
-	                       
-	                </div>
-		            </div>
-		            
-		        );
-    }
+	handleOpen() {
+		this.setState({
+			open: true,
+		});
+	}
+
+	routeRedirect() {
+		if(this.state.providers.PENDING) {
+			return <Redirect push to = {{
+				pathname: "/multiplephysicians",
+				state: {
+					fileUploadData: this.state.providers
+				}
+			}}
+			/>
+		}
+	}
+
+	render() {
+		return (
+			<div className="App-button">
+				<Button color="inherit" onClick={this.handleOpen.bind(this)}>
+					Add Multiple Physicians
+				</Button>
+				<DropzoneDialog
+					open={this.state.open}
+					onSave={this.handleSave.bind(this)}
+					acceptedFiles={['.xls', '.xlsx']}
+					dropzoneText={' Drag and drop an excel file here or click to upload '}
+					showPreviews={true}
+					maxFileSize={5000000}
+					onClose={this.handleClose.bind(this)}
+				/>
+				<div className="App-grid">               
+					{
+						this.routeRedirect()
+					}
+				</div>
+			</div>   
+		);
+	}
 }
